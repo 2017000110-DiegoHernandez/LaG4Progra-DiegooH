c65fdc8c2a428b644237480f4d0b7c1044f5c59c
2017000110-DiegoHernandez <dieguishernandez.g@gmail.com>
2026-05-10 16:17:52 -0600

    Add files via upload

---
+++ ejer11.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double a, b;
+
+        Console.WriteLine("Ingrese un número");
+        a = Convert.ToDouble(Console.ReadLine());
+
+        b = 1;
+        a = a + 1;
+
+        do
+        {
+            a = a - 1;
+            b = b * a;
+            Console.WriteLine(a);
+
+        }while (a != 1);
+        Console.WriteLine(b);
+
+    }
+}
