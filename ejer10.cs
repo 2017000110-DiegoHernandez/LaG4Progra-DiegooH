2783a2c09b5ddb8a4ccb390aec0a953dba03e419
2017000110-DiegoHernandez <dieguishernandez.g@gmail.com>
2026-05-10 16:25:00 -0600

    Add files via upload

---
+++ ejer10.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double a, b, c;
+        Console.WriteLine("Parqueo con 200 cupos");
+        a = 0;
+
+        do
+        {
+            a = a + 20;
+            Console.WriteLine("Cupos ocupados " + a);
+            Console.WriteLine("Espacio disponible");
+            Console.ReadKey();
+
+        } while (a != 180);
+
+        Console.WriteLine("Cupos ocupados 200");
+        Console.WriteLine("Espacio no disponible");
+
+    }
+}
