40d24c907131ae7bf4cf21151fe29819ce909782
2017000110-DiegoHernandez <dieguishernandez.g@gmail.com>
2026-05-10 19:02:20 -0600

    Add files via upload

---
+++ ejer 12.cs
@@ -0,0 +1,19 @@
+// ejer 12
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double a, b;
+        a = 101;
+
+        do
+        {
+            a = a - 2;
+            Console.WriteLine(a);
+
+        }while (a!=1);
+
+
+    }
+}
